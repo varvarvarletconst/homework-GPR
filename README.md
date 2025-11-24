155560e45f2184d2c2306e9a7ba3c54cbdae6392
Varvara <vako00007@stud.uni-saarland.de>
2025-11-24 03:16:55 +0100

    aufgabe 4

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # homework-GPR
 my GPR homework code
+README for the file pseudo-strFind:
+the aim is to recreate the function str.find() using only cycles, indexes, slicing and simple math. it finds the first index of the intersection of the string needle with a string haystack. it's not allowed to use str.xxx() 
+
+usage: 
+call out the function
+give the value of the needle variable -- that's the substring we are looking for
+give the value of the haystack variable -- that's the group of symbols (the string) where we are looking for the substring 
+the output is the least significant number of the needle in haystack or -1 if there is no such a intersection 
+
+for example:
+input:
+    needle: мама
+    haystack: амама
+output:
+    1
---
+++ pseudo-strFind.py
@@ -0,0 +1,20 @@
+needle = input('needle: ') #the part we are looking for
+haystack = input('haystack: ') #the group of symbols where we are looking for it
+#I found out that needl and haystack are traditional named for this type of exercises 
+
+def find_substring(needle, haystack):
+    if len(needle) == 0:
+        return 0
+    for i in range(len(haystack) - len(needle) + 1):
+        if haystack[i:i+len(needle)] == needle:
+            return i
+    return -1
+
+print(find_substring(needle, haystack))
+
+print('test 1: бал and хабалка')
+print(find_substring('бал', 'хабалка'))
+print('test 2: e and pi')
+print(find_substring('2.718281828459045', '3.141592653589793')) #our function cannot work with numbers so that's why we have to put our numbers in '' and those are actually strings not numbers
+print('test 3: ')
+print(find_substring('', 'hello'))
