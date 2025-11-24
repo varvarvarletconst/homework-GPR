4e6e79716b71d06bacb8e088ebf8524943b3e22e
Varvara <vako00007@stud.uni-saarland.de>
2025-11-24 01:53:27 +0100

    aufgabe 3

---
+++ string-function.py
@@ -0,0 +1,15 @@
+sequence = input('give me a string ')
+#a  
+print(len(sequence))
+#b 
+print(sequence.isalpha())
+#c 
+print(sequence.isidentifier())
+#d 
+print(sequence.endswith('ing')) 
+#e 
+print(sequence.lower())
+#or
+print(sequence.swapcase())
+#f 
+print(sequence.replace('a', 'e'))
